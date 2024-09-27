bb54a5627da6b83264ba92754e4ffe7ffbf267fe
Romuad-Zoungla <missimahouzoun@gmail.com>
2024-09-27 18:09:40 +0100

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export', // Ajoute cette ligne pour permettre l'exportation statique
 };
 
 export default nextConfig;
