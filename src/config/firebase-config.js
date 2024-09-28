fd13bfc3a0358fddd06671fdfcae78558e25d9f4
Romuad-Zoungla <missimahouzoun@gmail.com>
2024-09-28 10:09:08 +0100

    update

---
+++ src/config/firebase-config.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyC64l5k03jSQeF6yYZLdTajQSGMTPYGoPM",
+  authDomain: "codernextailds.firebaseapp.com",
+  projectId: "codernextailds",
+  storageBucket: "codernextailds.appspot.com",
+  messagingSenderId: "184366370908",
+  appId: "1:184366370908:web:63f2242ba49a10a65b9699"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
